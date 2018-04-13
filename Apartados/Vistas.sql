b708a49b9c513eef27c85ee9751f280a64d7bf4e
Isabella <irborrero@gmail.com>
2018-04-13 18:38:08 +0200

    Comienzo con tramos en vistas

--- Apartados/Vistas.sql
+++ Apartados/Vistas.sql
@@ -1,10 +1,27 @@
 
---DROP VIEW SancionesVelReducida;
+--DROP VIEW NuevaMulta;
 
-CREATE VIEW SancionesVelReducida AS
+CREATE VIEW NuevaMulta AS
 SELECT nPlate, odatetime, speed_limit - speed as diferencia from ROADS R INNER JOIN OBSERVATIONS O
     ON R.name=O.road
       WHERE R.speed_limit/2 > O.speed;
 
 
---select * from SancionesVelReducida;
+--select * from NuevaMulta;
+
+
+
+
+
+--DROP VIEW Tramos;
+
+CREATE VIEW Tramos AS
+SELECT name, km_point inicio, km_point+5 as final from ROADS R INNER JOIN OBSERVATIONS O
+    ON R.name=O.road
+      WHERE R.speed_limit/2 > O.speed;
+
+
+--select * from Tramos;
+
+
+SELECT road, km_point inicio, km_point+5 as fin, direction from RADARS where road='A5';
