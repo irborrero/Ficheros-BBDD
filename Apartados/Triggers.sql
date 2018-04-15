8efa35ad5180a3daa3c054f3e0d752ba0eed6d1b
Isabella <irborrero@gmail.com>
2018-04-15 13:01:11 +0200

    mismo que antes

--- Apartados/Triggers.sql
+++ Apartados/Triggers.sql
@@ -1,4 +1,6 @@
 
+------------------------------- TRIGGER A ----------------------------------
+----------- INSERCIÓN DE MULTA
 CREATE OR REPLACE TYPE OBSERVACION
 	AS OBJECT(
 			nPlate     VARCHAR2(7),
@@ -11,86 +13,7 @@ CREATE OR REPLACE TYPE OBSERVACION
    /
 
 
-create or replace trigger insertarMulta
-  AFTER insert on OBSERVATIONS
-    FOR EACH STATEMENT
-    DECLARE
-      amountVelMax NUMBER(3);
-      amountVelTramo NUMBER(3);
-      amountDist NUMBER(3);
-      obsAnterior OBSERVACION;
-
-    BEGIN
-
-        obsAnterior := OBSERVACION(NULL, NULL, NULL, NULL, NULL, NULL);
-        obsAnterior := ObservacionAnterior(:NEW.nPlate, :NEW.odatetime);
-        --amountVelMax := calculoVelMax(:NEW.nPlate, :NEW.odatetime);
-        --amountVelTramo := calculoVelTramo(:NEW.nPlate, obsAnterior.odatetime, :NEW.odatetime);
-        --amountDist := calculoSancionDistancia(:NEW.nplate, :NEW.odatetime);
-
-
-      DBMS_OUTPUT.PUT_LINE(amountVelMax || amountVelTramo || amountDist);
-    END;
-
-  /
-
-
-  create or replace trigger insertarMulta
-    FOR insert on OBSERVATIONS
-      COMPOUND TRIGGER
-
-        amountVelMax NUMBER(3);
-        amountVelTramo NUMBER(3);
-        amountDist NUMBER(3);
-        obsAnterior OBSERVACION;
-        -- Se lanzará después de cada fila actualizada
-      BEFORE EACH ROW IS
-      BEGIN
-          obsAnterior := OBSERVACION(NULL, NULL, NULL, NULL, NULL, NULL);
-          obsAnterior := paquete.ObservacionAnterior(:NEW.nPlate, :NEW.odatetime);
-          amountVelMax := paquete.calculoVelMax(:NEW.nPlate, :NEW.odatetime);
-          amountVelTramo := paquete.calculoVelTramo(:NEW.nPlate, obsAnterior.odatetime, :NEW.odatetime);
-          amountDist := paquete.calculoSancionDistancia(:NEW.nplate, :NEW.odatetime);
-      END BEFORE EACH ROW;
-
-      AFTER EACH ROW IS
-        DBMS_OUTPUT.PUT_LINE(amountVelMax || amountVelTramo || amountDist);
-
-      END AFTER EACH ROW;
-
-      END insertarMulta;
-    /
-
-
-
-
-    create or replace trigger insertarMulta FOR
-      insert on OBSERVATIONS
-        COMPOUND TRIGGER
-
-          amountVelMax NUMBER(3);
-          amountVelTramo NUMBER(3);
-          amountDist NUMBER(3);
-          obsAnterior OBSERVACION;
-
-
-        AFTER EACH ROW IS
-          BEGIN
-            obsAnterior := OBSERVACION(NULL, NULL, NULL, NULL, NULL, NULL);
-            obsAnterior := paquete.ObservacionAnterior(:OLD.nPlate, :OLD.odatetime);
-            amountVelMax := paquete.calculoVelMax(:OLD.nPlate, :OLD.odatetime);
-            amountVelTramo := paquete.calculoVelTramo(:OLD.nPlate, obsAnterior.odatetime, :OLD.odatetime);
-            amountDist := paquete.calculoSancionDistancia(:OLD.nplate, :OLD.odatetime);
-              DBMS_OUTPUT.PUT_LINE(amountVelMax || amountVelTramo || amountDist);
-          END AFTER EACH ROW;
-
-
-    END insertarMulta;
-
-      /
-
-
-			CREATE OR REPLACE TRIGGER MULTITA
+	CREATE OR REPLACE TRIGGER InsertarMulta
 				FOR INSERT ON OBSERVATIONS
 				COMPOUND TRIGGER
 				obsInsertada OBSERVACION;
@@ -107,9 +30,12 @@ create or replace trigger insertarMulta
 				amountDist NUMBER;
 				dueno VARCHAR(9);
 				obsAnterior OBSERVACION;
+				obsCocheAnterior OBSERVACION;
    		BEGIN
 				obsAnterior := OBSERVACION(NULL, NULL, NULL, NULL, NULL, NULL);
+				obsCocheAnterior := OBSERVACION(NULL, NULL, NULL, NULL, NULL, NULL);
 				obsAnterior := paquete.ObservacionAnterior(obsInsertada.nPlate, obsInsertada.odatetime);
+				obsCocheAnterior := paquete.ObservacionCocheAnterior(obsInsertada.nPlate, obsInsertada.odatetime);
 
 				amountVelMax := paquete.calculoVelMax(obsInsertada.nPlate, obsInsertada.odatetime);
 				amountVelTramo := paquete.calculoVelTramo(obsInsertada.nPlate, obsAnterior.odatetime, obsInsertada.odatetime);
@@ -118,6 +44,12 @@ create or replace trigger insertarMulta
 				DBMS_OUTPUT.PUT_LINE(amountVelMax || '-' || amountVelTramo||'-'||amountDist);
 
 				select owner into dueno from vehicles where nPlate = obsInsertada.nPlate;
+
+					--r: registrada
+					--i: emitida
+					--e: recibida
+					--f: abonada
+					--n: no abonada
 
 				IF amountVelMax > 0
 				THEN
@@ -128,15 +60,93 @@ create or replace trigger insertarMulta
 				IF amountvelTramo > 0
 				THEN
 				DBMS_OUTPUT.PUT_LINE('Insertando ticket por Vel Tramo...');
+				INSERT INTO TICKETS VALUES(obsInsertada.nPlate,obsAnterior.odatetime,'T',obsInsertada.nPlate,obsInsertada.odatetime,SYSDATE,NULL,NULL,amountVelTramo,dueno, 'R');
 				END IF;
 
 				IF amountDist > 0
 				THEN
 				DBMS_OUTPUT.PUT_LINE('Insertando ticket por distancia...');
+				INSERT INTO TICKETS VALUES(obsInsertada.nPlate,obsInsertada.odatetime,'D',obsCocheAnterior.nPlate,obsCocheAnterior.odatetime,SYSDATE,NULL,NULL,amountDist,dueno, 'R');
 				END IF;
 
    		END AFTER STATEMENT;
 
-END MULTITA;
+END InsertarMulta;
 
 /
+--------------------------------- TRIGGER C----------------------------------------
+CREATE OR REPLACE TRIGGER aReyMuerto
+BEfore UPDATE OF reg_driver on VEHICLES
+	DECLARE
+		IF re
+
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------- TRIGGER D----------------------------------------
+-------------- RESTRICCIONES
+--Restricciones: recoger que la velocidad de todo radar sea menor o igual que la general de la vía
+
+
+CREATE OR REPLACE TRIGGER NoInsertesRadar
+	FOR INSERT ON radars
+		COMPOUND trigger
+			speedRadar NUMBER;
+			speedRoad NUMBER;
+
+		AFTER EACH ROW IS
+			BEGIN
+				speedRadar := :NEW.speedlim;
+				select speed_limit into speedRoad from ROADS where name= :NEW.road;
+			END AFTER EACH ROW;
+
+		AFTER STATEMENT IS
+	 			velocidad_inadecuada EXCEPTION;
+			BEGIN
+				DBMS_OUTPUT.PUT_LINE(speedRadar || '-' || speedRoad);
+				IF speedRadar >= speedRoad THEN
+   				RAISE velocidad_inadecuada;
+				END IF;
+			EXCEPTION
+   			WHEN velocidad_inadecuada THEN
+					RAISE_APPLICATION_ERROR(-20001, 'La velocidad del radar debe ser inferior a la general');
+
+END AFTER STATEMENT;
+		END NoInsertesRadar;
+/
+
+
+-- y que los conductores tengan al menos 18 años.
+
+CREATE OR REPLACE TRIGGER NoInsertesConductor
+	BEFORE INSERT ON DRIVERS
+	FOR EACH ROW
+		DECLARE
+		cumple DATE;
+		edad number;
+		edad_inadecuada EXCEPTION;
+		BEGIN
+				select birthdate into cumple from persons where DNI = :NEW.dni;
+
+				edad := TRUNC(MONTHS_BETWEEN(SYSDATE, cumple))/12;
+
+				IF edad < 18 THEN
+					RAISE edad_inadecuada;
+				END IF;
+
+				EXCEPTION
+	   			WHEN edad_inadecuada THEN
+						RAISE_APPLICATION_ERROR(-20001, 'La edad del conductor debe ser superior a 18 años');
+	END;
+/
+--INSERT INTO RADARS VALUES ('A6','1','ASC','0');
+
+insert into PERSONS VALUES()
