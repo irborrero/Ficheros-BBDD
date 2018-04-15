cc463b0a9fff82efa0ece4bc822c7f51ca527f49
Isabella <irborrero@gmail.com>
2018-04-15 21:21:17 +0200

    Trigger que gestiona las alegaciones completado

--- Apartados/Triggers.sql
+++ Apartados/Triggers.sql
@@ -74,9 +74,52 @@ CREATE OR REPLACE TYPE OBSERVACION
 END InsertarMulta;
 
 /
+
+--------------------------------- TRIGGER B----------------------------------------
+---------- Si el nuevo deudor no es conductor asignado
+
+CREATE OR REPLACE TRIGGER ProcesarAlegacion
+BEFORE INSERT on ALLEGATIONS
+	FOR EACH ROW
+	DECLARE
+		c NUMBER;
+		deudor VARCHAR(9);
+		BEGIN
+			select count(*) into c from assignments where driver = :NEW.new_debtor and nPlate = :NEW.obs_veh;
+			select debtor into deudor from tickets where obs1_veh = :NEW.obs_veh and obs1_date =:NEW.obs_date and tik_type= :NEW.tik_type;
+
+			IF c = 0 THEN
+			----- El nuevo deudor no es conductor del coche
+				:NEW.status := 'R';
+				:NEW.exec_date := SYSDATE;
+
+			ELSE
+			---- Vemos si el nuevo deudor ya ha podido alegar esa misma multa
+				IF deudor NOT LIKE :NEW.new_debtor THEN
+						:NEW.status := 'A';
+						:NEW.exec_date := SYSDATE;
+				END IF;
+			END IF;
+	END;
+	/
+
+---------- Insertar un ticket
+--INSERT INTO TICKETS VALUES ('8489EAU','04/11/10 18:48:01,020000', 'S', NULL, NULL, SYSDATE, NULL, NULL, '30','97201505D','R');
+--INSERT INTO TICKETS VALUES ('9861AUO','13/01/11 05:57:33,510000', 'S', NULL, NULL, SYSDATE, NULL, NULL, '30','76150280V','R');
+
+			------ Alegación en la que el nuevo deudor no es conductor
+--INSERT INTO ALLEGATIONS VALUES('8489EAU', '04/11/10 18:48:01,020000', 'S', SYSDATE, '50774649X', 'U', NULL);
+
+		------ Alegación en la que el nuevo deudor no es conductor
+--INSERT INTO ASSIGNMENTS VALUES ('22117400W', '9861AUO');
+--INSERT INTO ALLEGATIONS VALUES('9861AUO', '13/01/11 05:57:33,510000', 'S', SYSDATE, '22117400W', 'U', NULL);
+
+
 --------------------------------- TRIGGER C----------------------------------------
+---------- Nuevo conductor habitual cuando el actual fallece para que el atributo no se quede a nulo
+
 CREATE OR REPLACE TRIGGER aReyMuerto
-BEfore UPDATE OF reg_driver on VEHICLES
+BEFRORE UPDATE OF REG_DRIVER on VEHICLES
 	DECLARE
 		IF re
 
@@ -93,38 +136,34 @@ BEfore UPDATE OF reg_driver on VEHICLES
 
 --------------------------------- TRIGGER D----------------------------------------
 -------------- RESTRICCIONES
---Restricciones: recoger que la velocidad de todo radar sea menor o igual que la general de la vía
 
+-- TRIGGER CONTROL DE LA VELOCIDAD DE LOS RADARES
 
 CREATE OR REPLACE TRIGGER NoInsertesRadar
-	FOR INSERT ON radars
-		COMPOUND trigger
+	BEFORE INSERT ON RADARS
+		FOR EACH ROW
+			DECLARE
 			speedRadar NUMBER;
 			speedRoad NUMBER;
-
-		AFTER EACH ROW IS
+			velocidad_inadecuada EXCEPTION;
 			BEGIN
 				speedRadar := :NEW.speedlim;
 				select speed_limit into speedRoad from ROADS where name= :NEW.road;
-			END AFTER EACH ROW;
-
-		AFTER STATEMENT IS
-	 			velocidad_inadecuada EXCEPTION;
-			BEGIN
-				DBMS_OUTPUT.PUT_LINE(speedRadar || '-' || speedRoad);
 				IF speedRadar >= speedRoad THEN
-   				RAISE velocidad_inadecuada;
+					RAISE velocidad_inadecuada;
 				END IF;
 			EXCEPTION
    			WHEN velocidad_inadecuada THEN
 					RAISE_APPLICATION_ERROR(-20001, 'La velocidad del radar debe ser inferior a la general');
-
-END AFTER STATEMENT;
-		END NoInsertesRadar;
+		END;
 /
+-------------------Velocidad adecuada ---------------------
+--INSERT INTO RADARS VALUES ('A4','0','ASC','0');
 
+-------------------Velocidad inadeucada ---------------------
+--INSERT INTO RADARS VALUES ('A4','0','ASC','200');
 
--- y que los conductores tengan al menos 18 años.
+-- TRIGGER CONTROL DE QUE LOS CONDUCTORES SEAN MAYORES DE EDAD
 
 CREATE OR REPLACE TRIGGER NoInsertesConductor
 	BEFORE INSERT ON DRIVERS
@@ -135,18 +174,21 @@ CREATE OR REPLACE TRIGGER NoInsertesConductor
 		edad_inadecuada EXCEPTION;
 		BEGIN
 				select birthdate into cumple from persons where DNI = :NEW.dni;
-
 				edad := TRUNC(MONTHS_BETWEEN(SYSDATE, cumple))/12;
-
 				IF edad < 18 THEN
 					RAISE edad_inadecuada;
 				END IF;
 
 				EXCEPTION
 	   			WHEN edad_inadecuada THEN
-						RAISE_APPLICATION_ERROR(-20001, 'La edad del conductor debe ser superior a 18 años');
+						RAISE_APPLICATION_ERROR(-20002, 'La edad del conductor debe ser superior a 18 años');
 	END;
 /
---INSERT INTO RADARS VALUES ('A6','1','ASC','0');
 
-insert into PERSONS VALUES()
+-------------- Menor de edad ----------------------
+--INSERT INTO PERSONS VALUES('1', 'Pablito', 'apellido1', 'apellido2', 'dir', 'ciudad', '123', 'blabla@bla','16/04/00');
+--INSERT INTO DRIVERS VALUES ('1', '16/04/00', 'A');
+
+-------------- Mayor de edad ----------------------
+--INSERT INTO PERSONS VALUES('2', 'Isabella', 'apellido1', 'apellido2', 'dir', 'ciudad', '123', 'blabla@bla','14/04/00');
+--INSERT INTO DRIVERS VALUES ('2', '16/04/00', 'A');
