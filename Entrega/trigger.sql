1fd6203b6e7726ec58b50b1bc03dc75bd1bbdadf
Isabella <irborrero@gmail.com>
2018-04-17 22:30:07 +0200

    TRIGGER OBLIGATORIO

---
+++ Entrega/trigger.sql
@@ -0,0 +1,134 @@
+---------------------------- OBLIGATORIO
+------------------------------- TRIGGER A ----------------------------------
+----------- INSERCIÓN DE MULTA
+CREATE OR REPLACE TYPE OBSERVACION
+	AS OBJECT(
+			nPlate     VARCHAR2(7),
+			odatetime  TIMESTAMP,
+			road       VARCHAR2(5),
+			km_point   NUMBER(3),
+			direction  VARCHAR2(3),
+			speed      NUMBER(3)
+		)
+   /
+
+
+	CREATE OR REPLACE TRIGGER InsertarMulta
+				FOR INSERT ON OBSERVATIONS
+				COMPOUND TRIGGER
+				obsInsertada OBSERVACION;
+			AFTER EACH ROW IS
+   		BEGIN
+				obsInsertada := OBSERVACION(NULL, NULL, NULL, NULL, NULL, NULL);
+				obsInsertada.nPlate := :NEW.nPlate;
+				obsInsertada.odatetime := :NEW.odatetime;
+   		END AFTER EACH ROW;
+
+   		AFTER STATEMENT IS
+				amountVelMax NUMBER;
+				amountVelTramo NUMBER;
+				amountDist NUMBER;
+				dueno VARCHAR(9);
+				obsAnterior OBSERVACION;
+				obsCocheAnterior OBSERVACION;
+   		BEGIN
+				obsAnterior := OBSERVACION(NULL, NULL, NULL, NULL, NULL, NULL);
+				obsCocheAnterior := OBSERVACION(NULL, NULL, NULL, NULL, NULL, NULL);
+				obsAnterior := paquete.ObservacionAnterior(obsInsertada.nPlate, obsInsertada.odatetime);
+				obsCocheAnterior := paquete.ObservacionCocheAnterior(obsInsertada.nPlate, obsInsertada.odatetime);
+
+				amountVelMax := paquete.calculoVelMax(obsInsertada.nPlate, obsInsertada.odatetime);
+				amountVelTramo := paquete.calculoVelTramo(obsInsertada.nPlate, obsAnterior.odatetime, obsInsertada.odatetime);
+				amountDist := paquete.calculoSancionDistancia(obsInsertada.nPlate, obsInsertada.odatetime);
+
+				DBMS_OUTPUT.PUT_LINE(amountVelMax || '-' || amountVelTramo||'-'||amountDist);
+
+				select owner into dueno from vehicles where nPlate = obsInsertada.nPlate;
+
+					--r: registrada
+					--i: emitida
+					--e: recibida
+					--f: abonada
+					--n: no abonada
+
+				IF amountVelMax > 0
+				THEN
+				DBMS_OUTPUT.PUT_LINE('Insertando ticket por Vel Max...');
+				INSERT INTO TICKETS VALUES(obsInsertada.nPlate,obsInsertada.odatetime,'S',NULL,NULL,SYSDATE,NULL,NULL,amountVelMax,dueno, 'R');
+				END IF;
+
+				IF amountvelTramo > 0
+				THEN
+				DBMS_OUTPUT.PUT_LINE('Insertando ticket por Vel Tramo...');
+				INSERT INTO TICKETS VALUES(obsInsertada.nPlate,obsAnterior.odatetime,'T',obsInsertada.nPlate,obsInsertada.odatetime,SYSDATE,NULL,NULL,amountVelTramo,dueno, 'R');
+				END IF;
+
+				IF amountDist > 0
+				THEN
+				DBMS_OUTPUT.PUT_LINE('Insertando ticket por distancia...');
+				INSERT INTO TICKETS VALUES(obsInsertada.nPlate,obsInsertada.odatetime,'D',obsCocheAnterior.nPlate,obsCocheAnterior.odatetime,SYSDATE,NULL,NULL,amountDist,dueno, 'R');
+				END IF;
+
+   		END AFTER STATEMENT;
+
+END InsertarMulta;
+
+/
+
+--------------------------------- TRIGGER B----------------------------------------
+---------- Si el nuevo deudor no es conductor asignado
+--- trigger a falta de revisión
+
+CREATE OR REPLACE TYPE ALEGACION
+	AS OBJECT(
+		obs_veh    VARCHAR2(7),
+    obs_date   TIMESTAMP,
+    tik_type   VARCHAR2(9),
+    reg_date   DATE,
+    new_debtor VARCHAR2(9),
+    status     VARCHAR2(1),
+    exec_date  DATE
+		)
+   /
+
+	CREATE OR REPLACE TRIGGER ProcesarAlegacion
+	 BEFORE INSERT on ALLEGATIONS
+			FOR EACH ROW
+			DECLARE
+				alegacionInsertada ALEGACION;
+				c1 NUMBER;
+				c2 NUMBER;
+
+			BEGIN
+				alegacionInsertada := ALEGACION(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+				alegacionInsertada.new_debtor := :NEW.new_debtor;
+				alegacionInsertada.obs_veh := :NEW.obs_veh;
+				alegacionInsertada.obs_date := :NEW.obs_date;
+				alegacionInsertada.tik_type := :NEW.tik_type;
+				alegacionInsertada.reg_date := :NEW.reg_date;
+
+				select count(*) into c1 from assignments where driver = alegacionInsertada.new_debtor and nPlate = alegacionInsertada.obs_veh;
+				--select debtor into deudor from tickets where obs1_veh = :NEW.obs_veh and obs1_date =:NEW.obs_date and tik_type= :NEW.tik_type;
+				select count(*) into c2 from allegations where obs_veh = alegacionInsertada.obs_veh and obs_date = alegacionInsertada.obs_date and tik_type = alegacionInsertada.tik_type and new_debtor = alegacionInsertada.new_debtor;
+
+				DBMS_OUTPUT.PUT_LINE(c1 || '-' || c2);
+
+				IF c1 = 0 THEN
+				-- El nuevo deudor no es conductor del coche
+					:NEW.status := 'R';
+					:NEW.exec_date := SYSDATE;
+
+				ELSE
+				---- Vemos si el nuevo deudor ya ha podido alegar esa multa
+					IF c2 = 0 THEN
+							--No ha alegado esa multa previamente
+							:NEW.status := 'A';
+							:NEW.exec_date := SYSDATE;
+					ELSE
+						--Ha alegado previamente
+						:NEW.status := 'U';
+						END IF;
+					END IF;
+
+		END ;
+		/
