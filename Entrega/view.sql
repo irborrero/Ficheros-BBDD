97869b65c646b607093655c06eb113caa517acdf
Isabella <irborrero@gmail.com>
2018-04-17 20:29:42 +0200

    VISTA OBLIGATORIA

---
+++ Entrega/view.sql
@@ -0,0 +1,23 @@
+
+---------- vista elegida como obligatoria: TRAMOS
+    CREATE VIEW TRAMOS AS
+      SELECT ROAD, KM_POINT,
+        CASE
+        WHEN (KM_POINT + 5 >= FINAL AND DIRECTION='ASC') THEN FINAL
+        WHEN (KM_POINT + 5 < FINAL AND DIRECTION='ASC') THEN KM_POINT +5
+        WHEN (KM_POINT - 5 >= FINAL AND DIRECTION='DES') THEN KM_POINT -5
+        WHEN (KM_POINT - 5 < FINAL AND DIRECTION='DES') THEN FINAL
+        END AS FINAL, SPEEDLIM FROM
+        (SELECT km_point, LEAD(KM_POINT, 1, KM_POINT +5) OVER (PARTITION BY ROAD ORDER BY KM_POINT) AS FINAl, ROAD, DIRECTION, SPEEDLIM FROM RADARS WHERE direction='ASC'
+        UNION ALL
+        SELECT KM_POINT,
+        CASE
+        WHEN (LEAD(KM_POINT, 1, KM_POINT -5) OVER (PARTITION BY ROAD ORDER BY km_point DESC)<0) THEN 0
+        ELSE LEAD(KM_POINT, 1, KM_POINT -5) OVER (PARTITION BY ROAD ORDER BY km_point DESC)
+        END AS FINAL, ROAD, DIRECTION, SPEEDLIM FROM RADARS WHERE direction='DES')
+        VISTA INNER JOIN ROADS R ON R.NAME= VISTA.ROAD WHERE VISTA.SPEEDLIM < R.speed_limit;
+
+        select  sum (velTramoPond) /sum(distTramo) from
+        (Select sum(abs(final - km_point)) as distTramo, (sum(abs(final - km_point)) * speedlim) as velTramoPond from Tramos where road = 'A5' group by speedlim);
+
+      
